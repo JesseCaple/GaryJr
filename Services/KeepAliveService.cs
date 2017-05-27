5f60a755fce99c18cdcfa7a4ed576cc2c39aaf42
Jesse Caple <thisrhythm@gmail.com>
2017-05-27 02:46:15 -0600

    now azurey

---
+++ Services/KeepAliveService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace GaryJr.Services
+{
+    class KeepAliveService
+    {
+        Timer timer;
+
+        KeepAliveService()
+        {
+            int ms = (int)TimeSpan.FromHours(12).TotalMilliseconds;
+            timer = new Timer(TimerCallback, null, 0, ms);
+        }
+
+        private static void TimerCallback(object o)
+        {
+            Console.WriteLine("Keep alive.");
+        }
+    }
+}
